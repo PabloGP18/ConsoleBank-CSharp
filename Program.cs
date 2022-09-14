7741be3e56dac0476a2f534fb79967b74aaa2a01
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-09-14 17:02:43 +0200

    Instantiate a client just to test my class outÃ

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data;
+using System.Runtime.InteropServices;
+using ConsoleBank_CSharp;
+
+
+Client client = new Client(1, "besart", DateTime.Now);
+Console.WriteLine("What is your name:");
+client.Name= Console.ReadLine();
+Console.WriteLine($"Your name is: {client.Name}");
