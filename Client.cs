e32de6b126d3b55a5672b8ea185b1bbadd91ade5
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-09-14 17:01:13 +0200

    made a class Client

--- Client.cs
+++ Client.cs
@@ -2,5 +2,32 @@
 
 public class Client
 {
-    
+    private int _id;
+    private string _name;
+    private DateTime _date;
+
+    public Client(int id, string name, DateTime date)
+    {
+        _id = id;
+        _name = name;
+        _date = date;
+    }
+
+    public int Id
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public DateTime Date
+    {
+        get => _date;
+        set => _date = value;
+    }
 }
