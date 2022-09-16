4a8b1071430f83dbde66b02f66b14acb38154101
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-09-16 18:40:05 +0200

    init the app

--- Program.cs
+++ Program.cs
@@ -3,7 +3,15 @@ using System.Runtime.InteropServices;
 using ConsoleBank_CSharp;
 
 
-Client client = new Client(1, "besart", DateTime.Now);
+/*Client client = new Client(1, "besart", DateTime.Now);
 Console.WriteLine("What is your name:");
 client.Name= Console.ReadLine();
-Console.WriteLine($"Your name is: {client.Name}");
+Console.WriteLine($"Your name is: {client.Name}");*/
+Bank bank = new Bank();
+/*bank.Registration();
+bank.Account();
+bank.Withdraw();
+bank.Deposit();*/
+bank.Registration();
+bank.Handler();
+
